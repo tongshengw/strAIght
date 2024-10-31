7436f93be1bba690c0bfb0cd7526f9491715007c
tswu <tswu@umich.edu>
2024-10-31 03:08:50 -0400

    render keypoints using canvas

--- frontend/straight/src/components/Camera.tsx
+++ frontend/straight/src/components/Camera.tsx
@@ -9,14 +9,26 @@ function Camera ({ isConnected }: { isConnected: boolean }) {
 
     const webcamRef = useRef<any>(null);
     const modelRef = useRef<poseDetection.PoseDetector | null>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const wrapperRef = useRef<HTMLDivElement>(null);
 
     // useEffect with [] as second parameter means the code is run once at initial render time
     useEffect(() => {
         const loadModel = async () => {
-            const detectorConfig = {modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING};
+            const detectorConfig = {modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER, minPoseScore: 0.3};
             modelRef.current = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig);
         }
         loadModel();
+
+        const interval = setInterval(
+            function() {
+                if (webcamRef.current) {
+                    console.log('image captured');
+                    capture();
+                }
+            }, 1000);
+        
+        return () => clearInterval(interval);
     }, []);
 
     // function takes in datauri imageSrc string, converts into HTMLimage, then runs model inference
@@ -28,6 +40,7 @@ function Camera ({ isConnected }: { isConnected: boolean }) {
                 if (modelRef.current) {
                     const poses = await modelRef.current.estimatePoses(img);
                     console.log(poses);
+                    displayPose(poses);
                     const postureData = processPoses(poses);
                     return postureData;
                 }
@@ -35,6 +48,30 @@ function Camera ({ isConnected }: { isConnected: boolean }) {
             
         }
         return null;
+    }
+
+    const displayPose = (poses: poseDetection.Pose[]) => {
+        const canvas = canvasRef.current;
+        if (canvas && wrapperRef.current) {
+            canvas.height = wrapperRef.current.clientHeight;
+            canvas.width = wrapperRef.current.clientWidth;
+        }
+        const ctx = canvas?.getContext("2d");
+        if (ctx && webcamRef.current && canvas) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.globalAlpha = 0.3;
+            const pose = poses[0];
+            if (pose) {
+                pose.keypoints.forEach((keypoint) => {
+                    if (keypoint && keypoint.score && keypoint.score > 0.3) {
+                        ctx.beginPath();
+                        ctx.arc(keypoint.x, keypoint.y, 5, 0, 2 * Math.PI);
+                        ctx.fillStyle = "red";
+                        ctx.fill();
+                    }
+                    });
+            }
+        }
     }
 
     
@@ -55,22 +92,13 @@ function Camera ({ isConnected }: { isConnected: boolean }) {
         [webcamRef]
     );
 
-
-    useEffect(() => {
-        const interval = setInterval(
-            function() {
-                if (webcamRef.current) {
-                    console.log('image captured');
-                    capture();
-                }
-            }, 1000);
-        
-        return () => clearInterval(interval);
-    }, []);
    
     return (
         <>
-            <Webcam mirrored ref={webcamRef} screenshotFormat="image/jpeg"/> 
+        <div ref = {wrapperRef} className="cameraWrapper" style={{position: 'relative'}}>
+            <Webcam mirrored ref={webcamRef} screenshotFormat="image/jpeg"/>
+            <canvas ref={canvasRef} style={{position: 'absolute', top: 0, left: 0}}/>
+        </div>
         </>
     );
 }
